996f9510d6580e6a227ed0ee0544ad5bc4a1e0be
sunder <evenquantity@gmail.com>
2024-04-10 15:44:13 +0300

    develop/dev04: done

--- develop/dev04/task.go
+++ develop/dev04/task.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
 /*
 === Поиск анаграмм по словарю ===
 
@@ -19,6 +25,79 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+type AnagramSet struct {
+	firstWord string
+	set       []string
+}
 
+func getAnagramMap(arr []string) map[string]AnagramSet {
+	hashMap := make(map[string]AnagramSet)
+
+	for _, word := range arr {
+		word = strings.ToLower(word)
+		// Get a key for hash map. Key is the word with sorted letters
+		keyWord := SortString(word)
+
+		anagramSet, ok := hashMap[keyWord]
+		if !ok {
+			// If there is no such set, then create a new set
+			hashMap[keyWord] = AnagramSet{
+				firstWord: word,
+				set:       []string{word},
+			}
+			continue
+		}
+
+		// Get an insert index for new word
+		idx := sort.SearchStrings(anagramSet.set, word)
+
+		// If the word already exists in set, then skip this iteration
+		if idx < len(anagramSet.set) && anagramSet.set[idx] == word {
+			continue
+		}
+
+		// Otherwise expand the set
+		anagramSet.set = append(anagramSet.set, "")
+
+		// If word needs to be inserted in the mid, then cut the set and shift the values on 1 position to the right
+		if idx < len(anagramSet.set) {
+			copy(anagramSet.set[idx+1:], anagramSet.set[idx:])
+		}
+
+		// Insert the word at index
+		anagramSet.set[idx] = word
+		// Update the set value in the map
+		hashMap[keyWord] = anagramSet
+	}
+
+	return hashMap
+}
+
+func formatAnagramMap(hashMap map[string]AnagramSet) map[string][]string {
+	resultMap := make(map[string][]string)
+
+	for _, set := range hashMap {
+		if len(set.set) != 1 {
+			resultMap[set.firstWord] = set.set
+		}
+	}
+
+	return resultMap
+}
+
+// GetAnagramSet will create a set of anagrams implemented using hash map
+//
+// Single anagrams will be skipped and not added to the result
+func GetAnagramSet(arr []string) map[string][]string {
+	hashMap := getAnagramMap(arr)
+
+	return formatAnagramMap(hashMap)
+}
+
+func main() {
+	anagrams := []string{"123", "231", "231", "kEk", "eKk", "single", "mmm", "mmm", "mmm"}
+
+	anagramSet := GetAnagramSet(anagrams)
+
+	fmt.Println(anagramSet)
 }
--- develop/dev04/task_test.go
+++ develop/dev04/task_test.go
@@ -1,1 +1,65 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetAnagramSet(t *testing.T) {
+	tests := []struct {
+		arr     []string
+		want    map[string][]string
+		wantErr bool
+	}{
+		{
+			nil,
+			map[string][]string{},
+			false,
+		},
+		{
+			[]string{"abc", "cba", "cbb", "asd"},
+			map[string][]string{
+				"abc": {"abc", "cba"},
+			},
+			false,
+		},
+		{
+			[]string{"123", "231", "231", "kEk", "eKk", "single", "Mmm", "mMm", "mmm"},
+			map[string][]string{
+				"123": {"123", "231"},
+				"kek": {"ekk", "kek"},
+			},
+			false,
+		},
+	}
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("Test case #%d", i), func(t *testing.T) {
+			got := GetAnagramSet(test.arr)
+
+			if len(got) != len(test.want) {
+				t.Errorf("GetAnagramSet() got len = %d, want len= %d", len(got), len(test.want))
+				return
+			}
+
+			for key, val := range got {
+				wantSet, ok := test.want[key]
+				if !ok {
+					t.Errorf("GetAnagramSet() got key = %s doesn't exists in want", key)
+					return
+				}
+
+				if len(val) != len(wantSet) {
+					t.Errorf("GetAnagramSet() got set len = %d, want set len = %d", len(val), len(wantSet))
+					return
+				}
+
+				for i := 0; i < len(val); i++ {
+					if val[i] != wantSet[i] {
+						t.Errorf("GetAnagramSet() got set value = %s, want set value = %s", val[i], wantSet[i])
+						return
+					}
+				}
+			}
+		})
+	}
+}
