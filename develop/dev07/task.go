12956eea7ec05cb0a266e37e48e60d8f14930d1d
sunder <evenquantity@gmail.com>
2024-04-17 09:29:47 +0300

    develop/dev07: done

--- develop/dev07/task.go
+++ develop/dev07/task.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 /*
 === Or channel ===
 
@@ -18,7 +23,7 @@ sig := func(after time.Duration) <- chan interface{} {
 		defer close(c)
 		time.Sleep(after)
 }()
-return c
+return c!
 }
 
 start := time.Now()
@@ -33,6 +38,42 @@ start := time.Now()
 fmt.Printf(“fone after %v”, time.Since(start))
 */
 
-func main() {
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	done := make(chan interface{})
+	for _, v := range channels {
+		// We run goroutine for each channel and wait for channel close
+		go func(c <-chan interface{}) {
+			select {
+			// If at least one channel is closed, then close done channel
+			case <-c:
+				close(done)
+			case <-done:
+				return
+			}
+		}(v)
+	}
+	return done
+}
 
+func main() {
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	start := time.Now()
+	<-or(
+		sig(1*time.Second),
+		sig(10*time.Second),
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(1*time.Hour),
+		sig(1*time.Minute),
+	)
+
+	fmt.Printf("Done after %v", time.Since(start))
 }
--- develop/dev07/task_test.go
+++ develop/dev07/task_test.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestOr(t *testing.T) {
+	tests := []struct {
+		channel chan interface{}
+	}{
+		{
+			make(chan interface{}),
+		},
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("Test #%d", i), func(t *testing.T) {
+			close(test.channel)
+			done := or(test.channel)
+
+			if _, ok := <-done; !ok {
+				return
+			} else {
+				t.Errorf("Channel is not closed!")
+				return
+			}
+		})
+	}
+}
